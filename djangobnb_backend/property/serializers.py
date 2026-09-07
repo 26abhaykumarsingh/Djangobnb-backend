54e1826d6c358f27302f6177abc6a17d0d9ed98b
Abhay <26abhaykumarsingh@gmail.com>
2026-09-07 15:59:46 +0530

    Get property detail from backend and connect property detail page

--- djangobnb_backend/property/serializers.py
+++ djangobnb_backend/property/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from useraccount.serializers import UserDetailSerializer
 
 from property.models import Property
 
@@ -7,3 +8,21 @@ class PropertiesListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Property
         fields = ["id", "title", "price_per_night", "image_url"]
+
+
+class PropertiesDetailSerializer(serializers.ModelSerializer):
+    landlord = UserDetailSerializer(read_only=True, many=False)
+
+    class Meta:
+        model = Property
+        fields = [
+            "id",
+            "title",
+            "description",
+            "price_per_night",
+            "image_url",
+            "bedrooms",
+            "bathrooms",
+            "guests",
+            "landlord",
+        ]
