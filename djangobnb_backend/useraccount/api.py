1714ab152f6d5ee247d2478468b0d714cc54e425
Abhay <26abhaykumarsingh@gmail.com>
2026-09-09 22:17:56 +0530

    Make myreservations dynamic

--- djangobnb_backend/useraccount/api.py
+++ djangobnb_backend/useraccount/api.py
@@ -1,4 +1,5 @@
 from django.http import JsonResponse
+from property.serializers import ReservationsListSerializer
 from rest_framework.decorators import (
     api_view,
     authentication_classes,
@@ -17,4 +18,11 @@ def landlord_detail(request, pk):
 
     serializer = UserDetailSerializer(user, many=False)
 
+    return JsonResponse(serializer.data, safe=False)
+
+
+@api_view(["GET"])
+def reservations_list(request):
+    reservations = request.user.reservations.all()
+    serializer = ReservationsListSerializer(reservations, many=True)
     return JsonResponse(serializer.data, safe=False)
