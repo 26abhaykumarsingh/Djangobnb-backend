40a198d881f0c759cf1627eb692e3969d2752da9
Abhay <26abhaykumarsingh@gmail.com>
2026-09-11 14:44:24 +0530

    Make inbox dynamic

---
+++ djangobnb_backend/chat/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from useraccount.serializers import UserDetailSerializer
+
+from .models import Conversation, ConversationMessage
+
+
+class ConversationListSerializer(serializers.ModelSerializer):
+    users = UserDetailSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Conversation
+        fields = (
+            "id",
+            "users",
+            "modified_at",
+        )
