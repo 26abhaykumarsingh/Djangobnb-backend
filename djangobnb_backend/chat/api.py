7d44b5bc54c04869a9ea5ddd18dab299caa4c398
Abhay <26abhaykumarsingh@gmail.com>
2026-09-16 14:36:20 +0530

    Start new conversation

--- djangobnb_backend/chat/api.py
+++ djangobnb_backend/chat/api.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
+from useraccount.models import User
 
 from .models import Conversation, ConversationMessage
 from .serializers import (
@@ -32,3 +33,22 @@ def conversations_detail(request, pk):
         },
         safe=False,
     )
+
+
+@api_view(["GET"])
+def conversations_start(request, user_id):
+    conversations = Conversation.objects.filter(users__in=[user_id]).filter(
+        users__in=[request.user.id]
+    )
+
+    if conversations.count() > 0:
+        conversation = conversations.first()
+
+        return JsonResponse({"succes": True, "conversation_id": conversation.id})
+    else:
+        user = User.objects.get(pk=user_id)
+        conversation = Conversation.objects.create()
+        conversation.users.add(request.user)
+        conversation.users.add(user)
+
+        return JsonResponse({"succes": True, "conversation_id": conversation.id})
